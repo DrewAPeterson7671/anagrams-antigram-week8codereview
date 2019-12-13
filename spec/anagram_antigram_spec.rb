14dde0d6a71812f449aea5331d287c5ab64af294
Student <example@example.com>
2019-12-13 10:36:18 -0800

    Third test if the first entered word has less letters passes

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -12,7 +12,7 @@ describe('Anagrams#compare_words') do
   end
   it("returns if the two input words are not anagrams, first word missing character") do
     anagrams = Anagrams.new("Silen", "Listen")
-    expect(anagrams.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: n"))
-  end  
+    expect(anagrams.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: t"))
+  end
 
 end
