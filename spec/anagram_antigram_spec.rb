336572936b31e6148565c27f69a8df759436c616
Student <example@example.com>
2019-12-13 10:19:31 -0800

    Pass test to identify failed match and return proper message

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -7,7 +7,7 @@ describe('Anagrams#compare_words') do
     expect(anagrams.compare_words()).to(eq("These words are anagrams"))
   end
   it("returns if the two input words are anagrams") do
-    anagrams = Anagrams.new("Silent", "Listen")
+    anagrams = Anagrams.new("Silent", "Liste")
     expect(anagrams.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: n"))
   end
 
