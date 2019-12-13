30f70134e9b62353c716798586c3b3cddf8473b1
Student <example@example.com>
2019-12-13 10:41:06 -0800

    fail test for case sensitivity

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -3,16 +3,19 @@ require('anagram_antigram')
 
 describe('Anagrams#compare_words') do
   it("returns if the two input words are anagrams") do
-    anagrams = Anagrams.new("Silent", "Listen")
+    anagrams = Anagrams.new("silent", "listen")
     expect(anagrams.compare_words()).to(eq("These words are anagrams"))
   end
   it("returns if the two input words are not anagrams, second word missing character") do
-    anagrams = Anagrams.new("Silent", "Liste")
-    expect(anagrams.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: n"))
+    anagrams2 = Anagrams.new("silent", "liste")
+    expect(anagrams2.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: n"))
   end
   it("returns if the two input words are not anagrams, first word missing character") do
-    anagrams = Anagrams.new("Silen", "Listen")
-    expect(anagrams.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: t"))
+    anagrams3 = Anagrams.new("silen", "listen")
+    expect(anagrams3.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: t"))
   end
-
+  it("test to see if case insensitive") do
+    anagrams3 = Anagrams.new("TeA", "Eat")
+    expect(anagrams3.compare_words()).to(eq("These words are not anagrams.  The letter(s) that are not in both words are: t"))
+  end
 end
