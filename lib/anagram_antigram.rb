e3164f4643b4e09a1143087c9add6af7af664045
Student <example@example.com>
2019-12-13 10:03:32 -0800

    Create a regular expression that will compare the two words and eliminate common letters

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,5 +1,23 @@
 class Anagrams
-  def compare_words(word1, word2)
-    return
+
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end
+
+  def compare_words
+    word_dif = @word1.gsub(/[#{@word2}]/i, '')
+    return word_dif
+    # word1_array = @word1.split()
+    # word2_array = @word2.split()
+    # puts word1_array
+    # puts word2_array
+  end
+
 end
+
+# return anagrams = Anagrams.new("silent", "listen")
+# @word1 = "Silent"
+# @word2 = "Listen"
+# word_dif = @word1.gsub(/[#{@word2}]/i, '')
+# return "liste".scan /Silent/i
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -2,8 +2,11 @@ require('rspec')
 require('anagram_antigram')
 
 describe('Anagrams#compare_words') do
-  if("returns if the two input words are anagrams")
-    anagrams = Anagrams.new()
-    expect(anagrams.compare_words("silent", "listen")).to(eq("These words are anagrams"))
-  end  
+  it("returns if the two input words are anagrams") do
+    anagrams = Anagrams.new("Silent", "Listen")
+    expect(anagrams.compare_words()).to(eq("These words are anagrams"))
+  end
+
+
+
 end
