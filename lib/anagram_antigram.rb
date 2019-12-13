1b66a174a811a3765303275f466b79c8b4d81d84
Student <example@example.com>
2019-12-13 15:40:41 -0800

    Refactor to move vowel and punctuation functionality to seperate methods

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,16 +1,13 @@
 class Anagrams
 
   def initialize(word1, word2)
-    @word1 = word1.gsub(/\W/, "")
-    @word2 = word2.gsub(/\W/, "")
+    @word1 = punctuation_remove(word1)
+    @word2 = punctuation_remove(word2)
   end
 
   def compare_words
 
-    vowel_check1 = @word1.match?(/[aeiouy]/i)
-    vowel_check2 = @word2.match?(/[aeiouy]/i)
-
-    if !vowel_check1 | !vowel_check2
+    if !vowel_check(@word1) | !vowel_check(@word2)
       return "You need to input actual words!"
     end
 
@@ -26,23 +23,11 @@ class Anagrams
     end
   end
 
-  # def punctuation_remove
-  #   @word1 = @word1.gsub(/\W]/, '')
-  # end
+  def punctuation_remove(punc_word)
+    return punc_word.gsub(/\W/, '')
+  end
 
-
-  # def vowel_checker?
-  #   vowel_test1 = ""
-  #   vowel_test2 = ""
-  #   vowel_test2 = @word2 =~ (/[aeiouy]/i)
-  #   vowel_test1 = @word1 =~ (/[aeiouy]/i)
-  #
-  #   puts vowel_test1
-  #   puts vowel_test2
-  #   # vowel_test = (@word1 =~ (/[aeiouy]/i)) + (@word2 =~ (/[aeiouy]/i))
-  #   # if vowel_test == Nil
-  #     return vowel_test
-  #   # end
-  # end
-
+  def vowel_check(vowel_word)
+    return vowel_word.match?(/[aeiouy]/i)
+  end
 end
