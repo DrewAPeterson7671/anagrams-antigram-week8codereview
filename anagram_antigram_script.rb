2f9459d186519189b92e796db25cdd55e14941dd
Student <example@example.com>
2019-12-13 16:16:04 -0800

    Added a script and tested it

--- anagram_antigram_script.rb
+++ anagram_antigram_script.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/ruby
 
 require ('./lib/anagram_antigram.rb')
+
+puts `clear`
+
+puts "**************************************************"
+puts "**************************************************"
+puts "Anagrams and Antigrams"
+puts "**************************************************"
+puts "**************************************************"
+puts "=================================================="
+
+puts "Please enter the first word."
+word1 = gets.chomp
+puts "Please enter the second word."
+word2 = gets.chomp
+
+
+anagram = Anagrams.new(word1, word2)
+puts "=================================================="
+puts "=================================================="
+p anagram.compare_words
+puts "=================================================="
+puts "=================================================="
